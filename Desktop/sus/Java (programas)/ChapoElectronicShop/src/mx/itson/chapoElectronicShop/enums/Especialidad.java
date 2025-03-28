5de29a599867838ebb434033b55183af084a45fd
Andrey-Enrique-GM <you@example.com>
2025-03-27 18:47:33 -0700

    Primer commit, olvide subir el proyecto a github antes de terminarlo ;)

---
+++ Desktop/sus/Java (programas)/ChapoElectronicShop/src/mx/itson/chapoElectronicShop/enums/Especialidad.java
@@ -0,0 +1,17 @@
+
+package mx.itson.chapoElectronicShop.enums;
+
+// @author Andrey
+
+public enum Especialidad 
+{
+    
+    ROBOTICA,
+    COMUNICACIONES,
+    ENERGIA,
+    ENERGIAS_RENOVABLES,
+    AUTOMATIZACION,
+    PROGRAMACION,
+    OTRO
+    
+}
---
+++ Desktop/sus/Java (programas)/ChapoElectronicShop/src/mx/itson/chapoElectronicShop/enums/TipoComponente.java
@@ -0,0 +1,15 @@
+
+package mx.itson.chapoElectronicShop.enums;
+
+// @author Andrey
+
+public enum TipoComponente 
+{
+    
+    RESISTENCIA,
+    CAPACITOR,
+    TRANSISTOR,
+    MICROCONTROLADOR,
+    OTRO
+    
+}
